41b581fe78e38f1dd3d41a6e367bf86214fb69b4
@stgferia <stgferia@gmail.com>
2023-06-01 21:45:27 -0600

    add new route signup

--- src/app/views/auth/auth-routing.module.ts
+++ src/app/views/auth/auth-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SignupComponent } from './signup/signup.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       { path: 'forgot-password', component: ForgotPasswordComponent },
+      { path: 'signup', component: SignupComponent },
       { path: '**', redirectTo:'signin' }
     ]
   }
