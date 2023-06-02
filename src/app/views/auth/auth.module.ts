ba227bbebdd6eb17ed53ae7665489784468c3296
Sanchez19 <camilaasanchez1903@gmail.com>
2023-06-01 21:01:38 -0600

    feat(signin):

--- src/app/views/auth/auth.module.ts
+++ src/app/views/auth/auth.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ForgotPasswordComponent } from './forgot-password/forgot-password.component';
+import { SigninComponent } from './signin/signin.component';
 
 @NgModule({
   declarations: [
-    ForgotPasswordComponent
+    ForgotPasswordComponent,
+    SigninComponent
   ],
   imports: [
     CommonModule
