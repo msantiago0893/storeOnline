0bdf3691b2c0a038e6a80c655e89370061f170d5
Marco Antonio <msantiago@linko.mx>
2023-09-30 11:52:41 -0600

    refactor(view-products): add products table

--- src/app/views/manager/category/view-products/view-products.component.ts
+++ src/app/views/manager/category/view-products/view-products.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { ActivatedRoute } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { loadProductsByCategory } from 'src/app/core/store/actions/category.action';
+import { selectCategory, selectProductByCategory } from 'src/app/core/store/selectors/category.selector';
 
 @Component({
   selector: 'app-view-products',
@@ -6,10 +12,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./view-products.component.sass']
 })
 export class ViewProductsComponent implements OnInit {
+  idCategory: any;
+  categorias$ = this.store.select(selectCategory);
+  products: Array<any> = [];
+  displayedColumns: string[] = ['name', 'price', 'description', 'creationAt'];
+  dataSource = new MatTableDataSource();
+  @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
 
-  constructor() { }
+  constructor(
+    private store: Store<any>,
+    private route: ActivatedRoute
+  ) {
+    this.idCategory = route.snapshot.params['id'];
+  }
 
   ngOnInit(): void {
+    this.getProducts();
+  }
+
+  ngAfterViewInit() {
+    if (this.paginator) {
+      this.dataSource.paginator = this.paginator;
+    }
+  }
+
+  getProducts() {
+    this.store.dispatch(loadProductsByCategory({id: this.idCategory}));
+
+    this.store.select(selectProductByCategory)
+      .subscribe((response: any) => {
+        this.dataSource.data = response;
+      });
   }
 
 }
