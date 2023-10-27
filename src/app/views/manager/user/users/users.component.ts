0e2c5c4ab0ccfece6e8ba781a8045c71ba7088cd
Marco Antonio <msantiago@linko.mx>
2023-10-26 20:52:27 -0600

    feat(manager): add users component

--- src/app/views/manager/user/users/users.component.ts
+++ src/app/views/manager/user/users/users.component.ts
@@ -4,22 +4,18 @@ import { MatTableDataSource } from '@angular/material/table';
 import { Store } from '@ngrx/store';
 import { deleteUser, loadUsers } from 'src/app/core/store/actions/user.action';
 import { selectUsers } from 'src/app/core/store/selectors/users.selector';
-import { fadeAnimation } from 'src/app/shared/animations/animation';
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.sass'],
-  animations: [
-    fadeAnimation
-  ]
+  styleUrls: ['./users.component.sass']
 })
 export class UsersComponent implements OnInit {
 
   displayedColumns: string[] = ['id', 'name', 'email', 'role', 'operation'];
   dataSource = new MatTableDataSource();
-
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  search:string = '';
 
   constructor(
     private store: Store<any>
@@ -33,6 +29,12 @@ export class UsersComponent implements OnInit {
     if (this.paginator) {
       this.dataSource.paginator = this.paginator;
     }
+  }
+
+  applyFilter() {
+    const filterValue = this.search.toLowerCase();
+    this.dataSource.filter = filterValue;
+    this.dataSource.paginator?.firstPage();
   }
 
   getUsers() {
