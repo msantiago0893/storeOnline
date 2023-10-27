44f4da230551b2d54c29916f5928e6f35aa02c97
Marco Antonio <msantiago@linko.mx>
2023-10-26 20:53:26 -0600

    feat(manager): add products component

--- src/app/views/manager/product/products/products.component.ts
+++ src/app/views/manager/product/products/products.component.ts
@@ -1,27 +1,55 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { MatPaginator } from '@angular/material/paginator';
 import { MatTableDataSource } from '@angular/material/table';
+import { Store } from '@ngrx/store';
+import { deleteProduct, loadProducts } from 'src/app/core/store/actions/product.action';
+import { selectProducts } from 'src/app/core/store/selectors/products.selector';
+import { fadeAnimation } from 'src/app/shared/animations/animation';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.sass']
+  styleUrls: ['./products.component.sass'],
+  animations: [
+    fadeAnimation
+  ]
 })
 export class ProductsComponent implements OnInit {
 
-  displayedColumns: string[] = ['id', 'name', 'creationAt', 'updatedAt', 'operation'];
+  displayedColumns: string[] = ['id', 'name', 'price', 'category', 'operation'];
   dataSource = new MatTableDataSource();
   @ViewChild(MatPaginator) paginator: MatPaginator | undefined;
+  search: string = '';
 
-  constructor() { }
+  constructor(
+    private store: Store<any>
+  ) { }
 
   ngOnInit(): void {
-    this.dataSource.data = [];
+    this.getProducts();
   }
 
   ngAfterViewInit() {
     if (this.paginator) {
       this.dataSource.paginator = this.paginator;
     }
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
+
+  getProducts() {
+    this.store.dispatch(loadProducts());
+
+    this.store.select(selectProducts)
+      .subscribe((products: any) => {
+        this.dataSource.data = products;
+      });
+  }
+
+  delete(id: number) {
+    this.store.dispatch(deleteProduct({id}));
   }
 }
