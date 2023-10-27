9499995277553003808b52a0b128ab6c14ab9325
Marco Antonio <msantiago@linko.mx>
2023-10-26 21:04:24 -0600

    feat(core): add selectors

--- src/app/core/store/selectors/category.selector.ts
+++ src/app/core/store/selectors/category.selector.ts
@@ -1,10 +1,8 @@
 import { createFeatureSelector, createSelector } from "@ngrx/store";
 import { CategoryState } from "../reducers/category.reducer";
 
-// Selector para obtener el estado de las categorías
 export const selectCategoryState = createFeatureSelector<CategoryState>('category');
 
-// Selector para obtener la lista de categorías
 export const selectCategories = createSelector(
   selectCategoryState,
   (state: CategoryState) => state.categories
